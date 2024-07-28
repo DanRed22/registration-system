2758f208366efb66d7e8a9083f8f7bcac75f7376
Angelou Sereño <angeloubendebelsereno@gmail.com>
2024-07-28 15:13:05 +0800

    added changes when adding users

--- src/components/AddModal.js
+++ src/components/AddModal.js
@@ -5,71 +5,60 @@ import API from '../components/Config'
 
 const AddModal = ({hide}) => {
 const [name, setName] = useState('');
-const [email, setEmail] = useState('');
 const [id, setId]= useState('');
-const [bibnum, setBibnum] = useState('');
-const [shirt, setShirt]  = useState('');
-const [stub, setStub] = useState('');
+const [additional, setAdditional] = useState('');
 const [remarks, setRemarks] = useState('');
+const [program, setProgram] = useState('');
 
 const handleNameChange = (e) =>{
     setName(e.target.value);
 }
 
-const handleEmailChange = (e)=>{
-    setEmail(e.target.value);
-}
 
 const handleIdChange = (e) =>{
     setId(e.target.value)
 }
 
-const handleBibnumChange = (e) =>{
-    setBibnum(e.target.value)
+
+const handleAdditionalChange = (e) =>{
+    setAdditional(e.target.value)
 }
 
-const handleStubChange = (e) =>{
-    setStub(e.target.value)
-}
 
-const handleShirtChange = (e) =>{
-    setShirt(e.target.value)
-}
 const handleIRemarksChange = (e) =>{
     setRemarks(e.target.value)
 }
 
+const handleProgramChange = (e) =>{
+    setProgram(e.target.value)
+}
 const handleSubmit = async ()=>{
     const response = await axios.post(`${API}add`, {
         "name": name,
-        "email": email,
         "id_number": id,
-        "number": bibnum,
-        "stub_number": stub,
-        "shirt_size": shirt,
+        "program": program,
+        "additional": additional,
         "remarks": remarks
     })
     if (response){
         alert(response.data.message);
     }
     setName('')
-    setEmail('')
     setId('')
-    setBibnum('')
-    setShirt('')
+    setAdditional('')
     setRemarks('')
-    setStub('')
+    setProgram('')
     hide()
 }
 
 
   return (
-    <div className='transition duration-100 w-[80vw] border border-solid border-white bg-black rounded-lg p-8 overflow-auto'>
+    <div className='w-[50rem] shadow-2xl bg-red-950 rounded-lg p-8 overflow-auto'>
         <div className='grid '>
         <button 
             onClick={hide} 
             type="button" 
-            class="justify-self-end text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-800 font-medium rounded-lg text-lg px-5 py-2.5 text-center me-2 mb-2"><FaWindowClose /></button>
+            className="flex transition duration-200 ease-in-out w-10 justify-self-end text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-800 font-medium rounded-lg text-lg items-center justify-center py-2.5 text-center"><FaWindowClose /></button>
         </div>
         <div class="grid gap-6 mb-6 md:grid-cols-2">
         <div>
@@ -77,25 +66,17 @@ const handleSubmit = async ()=>{
             <input value={name} onChange={handleNameChange} type="text" id="name" class="bg-gray-50 border border-gray-300  text-sm rounded-lg  block w-full p-2.5  placeholder-gray-400 text-black " placeholder="John Doe " />
         </div>
         <div>
-            <label for="email" class="block mb-2 text-sm font-medium  text-white">Email</label>
-            <input value={email} onChange={handleEmailChange} type="textarea" id="email" class="bg-gray-50 border border-gray-300  text-sm rounded-lg  block w-full p-2.5  placeholder-gray-400 text-black" placeholder="youremail@example.com" />
-        </div>
-        <div>
             <label for="id_number" class="block mb-2 text-sm font-medium  text-white">ID Number</label>
             <input value={id} onChange={handleIdChange} type="text" id="id_number" class="bg-gray-50 border border-gray-300  text-sm rounded-lg  block w-full p-2.5  placeholder-gray-400 text-black " placeholder="21100000" />
         </div>
         <div>
-            <label for="number" class="block mb-2 text-sm font-medium  text-white">BIB Number</label>
-            <input value={bibnum} onChange={handleBibnumChange} type="text" id="number" class="bg-gray-50 border border-gray-300  text-sm rounded-lg  block w-full p-2.5  placeholder-gray-400 text-black " placeholder="1" />
+            <label for="program" class="block mb-2 text-sm font-medium  text-white">Program</label>
+            <input value={program} onChange={handleProgramChange} type="textarea" id="program" class="bg-gray-50 border border-gray-300  text-sm rounded-lg  block w-full p-2.5  placeholder-gray-400 text-black" placeholder="Remarks Here" />
+        </div>
+        <div>
+            <label for="additional" class="block mb-2 text-sm font-medium  text-white">Additional Info</label>
+            <input value={additional} onChange={handleAdditionalChange} type="text" id="number" class="bg-gray-50 border border-gray-300  text-sm rounded-lg  block w-full p-2.5  placeholder-gray-400 text-black " placeholder="1" />
         </div>  
-        <div>
-            <label for="Shirt Size" class="block mb-2 text-sm font-medium  text-white">Shirt Size</label>
-            <input value={shirt} onChange={handleShirtChange} type="text" id="phone" class="bg-gray-50 border border-gray-300  text-sm rounded-lg  block w-full p-2.5  placeholder-gray-400 text-black " placeholder="(S, M, L, XL, XXL)"/>
-        </div>
-        <div>
-            <label for="stub_number" class="block mb-2 text-sm font-medium  text-white">Stub Number</label>
-            <input value={stub} onChange={handleStubChange} type="text" id="stub_number" class="bg-gray-50 border border-gray-300  text-sm rounded-lg  block w-full p-2.5  placeholder-gray-400 text-black " placeholder="20" />
-        </div>
         <div>
             <label for="remarks" class="block mb-2 text-sm font-medium  text-white">Remarks</label>
             <input value={remarks} onChange={handleIRemarksChange} type="textarea" id="remarks" class="bg-gray-50 border border-gray-300  text-sm rounded-lg  block w-full p-2.5  placeholder-gray-400 text-black" placeholder="Remarks Here" />
@@ -104,11 +85,9 @@ const handleSubmit = async ()=>{
         <div>
         
         </div>
-        <div>
-        <button onClick={handleSubmit} type="button" class="w-[20vw] focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Add Attendee</button>
-        </div>
         
     </div>
+    <button onClick={handleSubmit} type="button" class="w-[12rem] transition duration-200 focus:outline-none text-black hover:text-white bg-green-400 hover:bg-green-500 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Add Attendee</button>
     </div>
   )
 }
