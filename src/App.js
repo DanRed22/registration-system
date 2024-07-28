eb05e2168a3c9d02a53e29194abb41701403dc98
Angelou Sereño <angeloubendebelsereno@gmail.com>
2024-07-28 15:12:27 +0800

    new route for views

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import './App.css';
 
 import { Attendance } from "./pages/Attendance";
+import View from "./pages/View";
 function App() {
   return (
     <div className="App">
     <Router>
       <Routes>
         <Route path="/" element={<Attendance />} />
+        <Route path="/view" element={<View/>} />
       </Routes>
     </Router>
   </div>
