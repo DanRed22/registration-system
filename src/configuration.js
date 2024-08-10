753dc90bb9428ce4ca0dbad6438ed5d0fdac3bec
Angelou Sereño <angeloubendebelsereno@gmail.com>
2024-08-11 01:46:45 +0800

    system UI config

---
+++ src/configuration.js
@@ -0,0 +1,9 @@
+const config = {
+    websiteName: "Registration System",
+    year: 2024,
+    subheader: "CACI SSC | Aerolympics 2024",
+    eventName: "Aerolympics 2024",
+    database: "attendancesystem",
+}
+
+export default config;
