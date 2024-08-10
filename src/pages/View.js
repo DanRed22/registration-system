4ab9357c73bf477498246e0d9f1861af4c6c616e
Angelou Sereño <angeloubendebelsereno@gmail.com>
2024-08-11 01:47:31 +0800

    added button

--- src/pages/View.js
+++ src/pages/View.js
@@ -1,17 +1,29 @@
 import React, {useState} from 'react'
 import ViewTable from '../components/ViewTable'
 import Notification from '../components/Notification';
+import { useNavigate } from 'react-router-dom';
 export default function View() {
+  const navigate = useNavigate();
   const [showNotif, setShowNotif] = useState(false);
   const [message, setMessage] = useState('');
   
+  const goHome = () =>{
+    navigate("/");
+  }
   const hideNotif = () =>{
     setShowNotif(false);
     //console.log("closed notif")
 }
   return (
-    <div className='flex items-center justify-center '>
+    <div className='flex items-center flex-col justify-center '>
       {showNotif && <Notification hideNotif={hideNotif} message={message}/>}
+
+      <button 
+        className='p-2 bg-green-500 m-2 rounded-md text-white transition duration-200 font-bold hover:bg-green-800 hover:shadow-lg'
+        onClick={goHome}
+        >
+        
+          Go Home</button>
       <ViewTable showNotif={setShowNotif} setMessage={setMessage} />
     </div>
   )
