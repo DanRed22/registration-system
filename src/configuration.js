0baa6c2f0102aecf7e47364b428e1c5055fe28af
Angelou Sereño <angeloushatchitsolutions@gmail.com>
2024-11-08 00:55:00 +0800

    config restructure

--- src/configuration.js
+++ src/configuration.js
@@ -1,20 +1,13 @@
 const config = {
-    websiteName: 'Registration System',
-    year: 2024,
-    subheader: 'CACI SSC | Aerolympics 2024',
-    eventName: 'Aerolympics 2024',
-    database: 'attendancesystem',
-    password: 'reset123',
-    websiteName: process.env.WEBSITENAME || 'Registration System',
-    year: process.env.YEAR || 2024,
-    subheader: process.env.SUBHEADER || 'CACI SSC | Aerolympics 2024',
-    eventName: process.env.EVENTNAME || 'Aerolympics 2024',
-    database: process.env.DATABASE || 'attendancesystem',
-    password: process.env.PASSWORD || 'reset123',
-    amount_1_name: process.env.AMOUNT_1_NAME || 'SSC Fee',
-    amount_2_name: process.env.AMOUNT_2_NAME || 'Aquaintance Party Fee',
-    amount_1_name: 'SSC Fee',
-    amount_2_name: 'Aquaintance Party Fee',
+    websiteName: process.env.REACT_APP_WEBSITENAME || 'Registration System',
+    year: process.env.REACT_APP_YEAR || 2024,
+    subheader: process.env.REACT_APP_SUBHEADER || 'CACI SSC | Aerolympics 2024',
+    eventName: process.env.REACT_APP_EVENTNAME || 'Aerolympics 2024',
+    database: process.env.REACT_APP_DATABASE || 'attendancesystem',
+    password: process.env.REACT_APP_PASSWORD || 'reset123',
+    amount_1_name: process.env.REACT_APP_AMOUNT_1_NAME || 'SSC Fee',
+    amount_2_name:
+        process.env.REACT_APP_AMOUNT_2_NAME || 'Aquaintance Party Fee',
 };
 
 export default config;
